381a4508c9113104d1cc03b030aad687cee4303f
colderry <colderryz@gmail.com>
2021-12-29 02:04:07 +0600

    Datatypes

---
+++ datatypes.lua
@@ -0,0 +1,16 @@
+-- type() can be used for knowing a variable datatype
+-- Semicolon are useless and I'm using because habit
+
+local null = nil;
+print(type(null), "- No data.");
+
+local name = "Colderry";
+print(type(name), "- Represents array of characters.");
+
+local number = 47;
+print(type(number), "- Represnets numbers.")
+
+local developer = true;
+print(type(developer), "True and False as variables. Generally used for condition checking.");
+
+print(type(print), "- Represents a method that is written in C or Lua.")
